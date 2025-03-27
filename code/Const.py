2839e9a33a5b2c6d35ab27e44ab4425f901e3748
Davis <dls.lopes.silva@outlook.com>
2025-03-26 22:49:19 -0300

    Created navigation between menus Created rainbow in the menu

--- code/Const.py
+++ code/Const.py
@@ -1,6 +1,25 @@
+import colorsys
+import random
+
+
+def gerar_cor_rainbow():
+    hue = random.random()
+
+    saturation = 1
+    lightness = 0.5
+
+    r, g, b = colorsys.hsv_to_rgb(hue, saturation, lightness)
+    return int(r * 255), int(g * 255), int(b * 255)  # RGB
+
+
+
 #C
 COLOR_ORANGE = (255, 100, 50)
 COLOR_WHITE = (255, 255, 255)
+COLOR_RAINBOW = gerar_cor_rainbow()
+
+
+
 #M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
--- code/menu.py
+++ code/menu.py
@@ -1,37 +1,79 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import colorsys
+import random
+
 import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_RAINBOW
 
+COLOR_RAINBOW = (255, 255, 255)
 
 class Menu:
+
     def __init__(self, window):
         self.window = window
         self.surf = pygame.image.load('./asset/MenuBg.png')
         self.rect = self.surf.get_rect(left=0, top=0)
 
+    def gerar_cor_rainbow(self):
+        hue = random.random()
+
+        saturation = 1
+        lightness = 0.5
+
+        r, g, b = colorsys.hsv_to_rgb(hue, saturation, lightness)
+        return int(r * 255), int(g * 255), int(b * 255)  # RGB
+
+    def atualizar_cor_rainbow(self):
+        global COLOR_RAINBOW
+        COLOR_RAINBOW = self.gerar_cor_rainbow()
+
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./asset/MenuMusic.mp3')
         pygame.mixer_music.play(-1)
         while True:
 
+            # DRAW IMAGES
+
+            self.atualizar_cor_rainbow()
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, 'Shooter', (COLOR_ORANGE), ((WIN_WIDTH / 2), 25))
             self.menu_text(50, 'City', (COLOR_ORANGE), ((WIN_WIDTH / 2), 80))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 150 + 30 * i))
-
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_RAINBOW, ((WIN_WIDTH / 2), 150 + 30 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 150 + 30 * i))
             pygame.display.flip()
 
             # Check for all events
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close Window
                     quit()  # End pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN: # KEYDOWN
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP: #KEYUP
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN: # ENTER
+                        return MENU_OPTION[menu_option]
+
+
+
+
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
