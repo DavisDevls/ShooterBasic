bdce49bf58253fe59a027f94e74f1c13877c5477
Davis <dls.lopes.silva@outlook.com>
2025-03-26 23:48:00 -0300

    Created ABC class Created Factory Level 1 Bg implementation: parallax

--- code/Const.py
+++ code/Const.py
@@ -18,7 +18,15 @@ COLOR_ORANGE = (255, 100, 50)
 COLOR_WHITE = (255, 255, 255)
 COLOR_RAINBOW = gerar_cor_rainbow()
 
+#E
 
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+}
 
 #M
 MENU_OPTION = ('NEW GAME 1P',
